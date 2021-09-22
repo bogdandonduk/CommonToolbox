a6e3de0c1f2046dd60204f7882b2967aaababe7c
bogdandonduk <bogdan.donduk@gmail.com>
2021-09-22 17:13:10 +0700

    Changed CommonToolbox#hideSystemUI() implementation

--- CommonToolboxAndroidLib/src/main/java/bogdandonduk/commontoolboxlib/CommonToolbox.kt
+++ CommonToolboxAndroidLib/src/main/java/bogdandonduk/commontoolboxlib/CommonToolbox.kt
@@ -217,11 +217,11 @@ object CommonToolbox {
 
     fun hideSystemUI(window: Window) {
         window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+                and View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                and View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                and View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                and View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                and View.SYSTEM_UI_FLAG_FULLSCREEN)
     }
 
     fun showSystemUI(window: Window) {
